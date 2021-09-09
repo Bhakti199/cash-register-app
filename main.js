27d822b9de157b98c9275014eedb4124e2419b61
Bhakti199 <78597483+Bhakti199@users.noreply.github.com>
2021-09-09 17:31:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,24 +3,34 @@ const cashGiven = document.querySelector("#cash-input");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const cashTable = document.querySelector("#show-cash-table");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click",validateBillAndCashAmount);
 function validateBillAndCashAmount()
 {
-//    hideMessage();
+   hideMessage();
 const bill = Number(billAmount.value);
 const cash = Number(cashGiven.value);
-  if (bill > 0) {
+  if (bill > 0 && cash > 0) {
     if (cash > bill) {
       const amountToBeReturned = cash - bill;
       calculateChange(amountToBeReturned);
-    } else {
-     message.innerText = "Do you wanna wash plates??";
+      cashTable.style.display = "flex";
+    } 
+    if(cash === bill){
+         showMessage("bill paid");
+         cashTable.style.display = "none";
+    }
+    if(cash < bill){
+        showMessage("Do you wanna wash plates");
+        cashTable.style.display = "none";
     }
   } else {
-    message.innerText=" Invalid bill amount.";
+    showMessage(" Invalid input.");
+    cashTable.style.display = "none";
+    
   }
 };
 
@@ -31,7 +41,10 @@ function calculateChange(amountToBeReturned) {
     noOfNotes[i].innerText = numberOfNotes;
   }
 }
-
-//  function hideMessage() {
-//    message.style.display = "none";
-// }
+function showMessage(msg){
+    message.style.display= "";
+    message.innerText = msg;
+}
+ function hideMessage() {
+   message.style.display = "none";
+}
